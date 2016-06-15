8ebf5c1adf60b5e9d51f460f5803460a4000a699
Choden <stck@outlook.com>
2016-06-14 21:52:59 -0700

    Initial commit

---
+++ spec/markdown-todo-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import MarkdownTodoView from '../lib/markdown-todo-view';
+
+describe('MarkdownTodoView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
